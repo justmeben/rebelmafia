47332ec18e5354c6efbe3cfc59de51f721a6ca27
Bentzy <ben.ro@gloat.com>
2022-11-05 01:45:35 +0200

    configured urls home page infra

--- leagues/urls.py
+++ leagues/urls.py
@@ -16,5 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from leagues import views
+
 urlpatterns = [
+    path('', views.HomeView.as_view())
 ]
