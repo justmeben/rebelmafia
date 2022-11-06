1e0b21e6758c097c206e916ae9b24a88fc730fe6
Bentzy <ben.ro@gloat.com>
2022-11-07 01:03:49 +0200

    added winrate and format nicely

--- leagues/service.py
+++ leagues/service.py
@@ -10,7 +10,7 @@ def get_league_ranking_table(league: League) -> List:
             players_data[result.player_id] = dict(id=result.player_id, nickname=result.player.nickname,
                                                   position=0, total_points=0, total_extra_points=0,
                                                   total_plus_points=0, total_minus_points=0, wins=0, losses=0,
-                                                  total_games=0)
+                                                  total_games=0, winrate=0)
         players_data[result.player_id]['total_points'] += result.win_score + result.extra_score
         players_data[result.player_id]['total_extra_points'] += result.extra_score
         players_data[result.player_id]['total_plus_points'] += result.plus_score
@@ -20,6 +20,7 @@ def get_league_ranking_table(league: League) -> List:
         else:
             players_data[result.player_id]['losses'] += 1
         players_data[result.player_id]['total_games'] += 1
+        players_data[result.player_id]['winrate'] = players_data[result.player_id]['wins'] / players_data[result.player_id]['total_games']
 
     ranking_table = sorted(players_data.values(), key=lambda x: x.get('total_points', -1), reverse=True)
     for i in range(len(ranking_table)):
